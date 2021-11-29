88961951ffb97a3f69184a7e95a8c1dbc87e5d2d
hameddeveloper64 <hamed.developer.64@gmail.com>
2021-11-29 17:50:51 +0330

    first commit project add

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use Hameddeveloper64\library\Persian_date;
+$date = new Persian_date();
+echo $date->today();
+
+?>
---
+++ src/library/Persian_date.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Hameddeveloper64\library;
+
+/**
+ *
+ */
+class Persian_date
+{
+
+  public function today(){
+    return date("Y-m-d");
+  }
+}
+
+
+ ?>
